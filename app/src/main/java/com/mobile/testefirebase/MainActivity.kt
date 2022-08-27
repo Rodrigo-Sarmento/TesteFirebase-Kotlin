3273c0919483481622fe080d6d5e194bdd783ecc
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2022-08-27 16:07:26 -0300

    inicial

---
+++ app/src/main/java/com/mobile/testefirebase/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mobile.testefirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
