3655c5ed080e5f3a26d5e6fd802f5fa9495581ef
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2022-08-29 16:47:45 -0300

    gravar no DB

--- app/src/main/java/com/mobile/testefirebase/view/telaprincipal/TelaPrincipal.kt
+++ app/src/main/java/com/mobile/testefirebase/view/telaprincipal/TelaPrincipal.kt
@@ -3,7 +3,9 @@ package com.mobile.testefirebase.view.telaprincipal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.mobile.testefirebase.R
 import com.mobile.testefirebase.databinding.ActivityTelaPrincipalBinding
 import com.mobile.testefirebase.view.formlogin.FormLogin
@@ -11,6 +13,8 @@ import com.mobile.testefirebase.view.formlogin.FormLogin
 class TelaPrincipal : AppCompatActivity() {
 
     private lateinit var binding: ActivityTelaPrincipalBinding
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTelaPrincipalBinding.inflate(layoutInflater)
@@ -21,6 +25,21 @@ class TelaPrincipal : AppCompatActivity() {
             val voltarTelaLogin = Intent(this, FormLogin::class.java)
             startActivity(voltarTelaLogin)
             finish()
+        }
+
+        binding.btGravarDadosDB.setOnClickListener {
+
+            val usuariosMap = hashMapOf(
+                "nome" to "Marcos",
+                "sobrenome" to "Duarte Gomes",
+                "idade" to 28
+            )
+            db.collection("Usuários").document("Marcos").set(usuariosMap)
+                .addOnCompleteListener {
+                    Log.d("db", "Sucesso ao salvar os dados do usuário!")
+                }.addOnFailureListener{
+
+                }
         }
     }
 }
